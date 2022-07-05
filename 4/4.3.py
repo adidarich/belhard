b91b52a39d7611e3e4b8d885d0794ac781eaab7c
adidarich <adidarich1994@gmail.com>
2022-07-05 17:32:56 +0300

    changed 4.3 hw

--- 4/4.3.py
+++ 4/4.3.py
@@ -2,9 +2,11 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 
-dict = {0: input('name '),
-        1: input('email ')
+dictionary = {0: input('name: '),
+              1: input('email: ')
 
 }
-print(dict.keys())
-print(dict.values())
+name = {i: 'name' for i in range(1, 11)}
+email = {a: 'email' for a in range(1, 11)}
+
+print(name, email)
